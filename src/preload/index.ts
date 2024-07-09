a4a8b197bd27ea526f6a66d0cf5a0335f83734e5
Éricles Miller <ericlesmiller15@gmail.com>
2024-07-09 13:08:26 -0300

    feat: first commit

---
+++ src/preload/index.ts
@@ -0,0 +1,82 @@
+import { contextBridge } from 'electron'
+import { electronAPI } from '@electron-toolkit/preload'
+import { Employees } from './employees'
+
+// Custom APIs for renderer
+const api = {}
+
+const employees = new Employees('mongodb://localhost:27017', 'teste')
+let gotEmployeeCallback
+let gotEmployeeUpdatedCallback
+let gotDeletedResultCallback
+
+const getEmployees = () => {
+  console.log(`mainPreload > getEmployees`)
+
+  employees.getEmployees().then((res) => {
+    gotEmployeeCallback(res)
+  })
+}
+
+const gotEmployees = (callback) => {
+  gotEmployeeCallback = callback
+}
+
+const saveEmployee = (employee) => {
+  console.log(
+    `mainPreload > Salary: ${employee.salary}, Name: ${employee.name}, Position: ${employee.position}`
+  )
+  return employees.addEmployee(employee)
+}
+
+const deleteEmployees = (id) => {
+  console.log(`mainPreload > Delete : ${id}`)
+
+  employees.deleteEmployee(id).then((res) => {
+    gotDeletedResultCallback(res)
+  })
+}
+
+const gotDeletedResult = (callback) => {
+  gotDeletedResultCallback = callback
+}
+
+const updateEmployee = (id, emp) => {
+  console.log(`mainPreload > upDateEmployee : ${id}`)
+
+  const employee = {
+    salary: emp.salary,
+    name: emp.name,
+    position: emp.position
+  }
+
+  employees.updateEmployee(id, employee).then((res) => {
+    gotEmployeeUpdatedCallback(res)
+  })
+}
+
+const gotEmployeeUpdatedResult = (callback) => {
+  gotEmployeeUpdatedCallback = callback
+}
+
+if (process.contextIsolated) {
+  try {
+    contextBridge.exposeInMainWorld('electron', electronAPI)
+    contextBridge.exposeInMainWorld('api', {
+      getEmployees,
+      gotEmployees,
+      saveEmployee,
+      updateEmployee,
+      gotEmployeeUpdatedResult,
+      gotDeletedResult,
+      deleteEmployees
+    })
+  } catch (error) {
+    console.error(error)
+  }
+} else {
+  // @ts-ignore (define in dts)
+  window.electron = electronAPI
+  // @ts-ignore (define in dts)
+  window.api = api
+}
---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+import { EmployeeManager } from './components/Employee'
+
+function App(): JSX.Element {
+  return (
+    <>
+      <EmployeeManager></EmployeeManager>
+    </>
+  )
+}
+
+export default App
